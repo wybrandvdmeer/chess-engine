f844feecbdd69e032d9d24f22eb70897bda046c6
wybrand <wybrandvdmeer@gmail.com>
2024-05-03 17:13:38 +0200

    jj

--- UciInterpreter.cpp
+++ UciInterpreter.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 #include "UciInterpreter.h"
 #include "UciCommand.h"
 
 using namespace std;
 
+std::vector<std::string> uciCommands = { "position", "go", "isready", "uci" };
+
 UciCommand * UciInterpreter::read() {
 	std::string line; 
 	if(std::getline(std::cin, line)) {
+		stringstream ss(line);
 
-		cout << line;
+		std::string firstWord;
+		ss >> firstWord;
+
+		if(std::find(uciCommands.begin(), uciCommands.end(), firstWord) == uciCommands.end()) {
+			cout << "Unknown command"; 
+		} else {
+		}
 	}
 	return NULL;
 }
